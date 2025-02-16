d6a4c928fe3e7075b96822d781c96ad956f96502
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-02-16 16:15:48 +0100

    Basic graph structure

---
+++ src/graph.rs
@@ -0,0 +1,111 @@
+// (c) Copyright 2025 Mikołaj Kuranowski
+// SPDX-License-Identifier: MIT
+
+use crate::{Edge, Node};
+use std::collections::btree_map::{BTreeMap, Entry};
+
+/// Represents an OpenStreetMap network as a set of [Nodes](Node)
+/// and [Edges](Edge) between them.
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct Graph(BTreeMap<i64, (Node, Vec<Edge>)>);
+
+impl Graph {
+    /// Returns the number of nodes in the graph.
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    /// Returns an iterator over all [Nodes](Node) in the graph.
+    pub fn iter(&self) -> impl Iterator<Item = &Node> {
+        self.0.iter().map(|(_, (node, _))| node)
+    }
+
+    /// Retrieves a [Node] with the provided id.
+    pub fn get_node(&self, id: i64) -> Option<Node> {
+        self.0.get(&id).map(|&(node, _)| node)
+    }
+
+    /// Creates or updates a [Node] with `node.id`.
+    ///
+    /// All outgoing and incoming edges are preserved.
+    /// Updating a [Node] position might result in violation of the
+    /// [Edge] cost invariant (and thus break route finding) and
+    /// is therefore disallowed.
+    pub fn set_node(&mut self, node: Node) {
+        match self.0.entry(node.id) {
+            Entry::Vacant(e) => {
+                e.insert((node, Vec::default()));
+            }
+            Entry::Occupied(mut e) => {
+                debug_assert_eq!(e.get().0.id, node.id);
+                e.get_mut().0 = node;
+            }
+        }
+    }
+
+    /// Deletes a [Node] with a given `id`.
+    ///
+    /// While all outgoing edges are removed, incoming edges are preserved
+    /// (as this would require a walk over all nodes in the graph).
+    /// Thus, deleting a node and then re-using its id might result in violation
+    /// of the [Edge] cost invariant (and break route finding) is disallowed.
+    pub fn delete_node(&mut self, id: i64) {
+        self.0.remove(&id);
+    }
+
+    /// Gets all outgoing [Edges](Edge) from a node with a given id.
+    pub fn get_edges(&self, from_id: i64) -> &[Edge] {
+        self.0
+            .get(&from_id)
+            .map(|(_, e)| e.as_slice())
+            .unwrap_or_default()
+    }
+
+    /// Gets the cost of an [Edge] from one node to another.
+    /// If such an edge doesn't exist, returns [f32::INFINITY].
+    pub fn get_edge(&self, from_id: i64, to_id: i64) -> f32 {
+        self.0
+            .get(&from_id)
+            .map(|(_, e)| {
+                e.iter().find_map(|edge| {
+                    if edge.to == to_id {
+                        Some(edge.cost)
+                    } else {
+                        None
+                    }
+                })
+            })
+            .flatten()
+            .unwrap_or(f32::INFINITY)
+    }
+
+    /// Creates or updates an [Edge] from a node with a given id.
+    pub fn set_edge(&mut self, from_id: i64, edge: Edge) {
+        if let Some((_, edges)) = self.0.get_mut(&from_id) {
+            if let Some(candidate) = edges.iter_mut().find(|e| e.to == edge.to) {
+                *candidate = edge;
+            } else {
+                edges.push(edge);
+            }
+        }
+    }
+
+    /// Removes an edge from one node to another.
+    pub fn delete_edge(&mut self, from_id: i64, to_id: i64) {
+        if let Some((_, edges)) = self.0.get_mut(&from_id) {
+            if let Some(idx) =
+                edges.iter().enumerate().find_map(
+                    |(idx, edge)| {
+                        if edge.to == to_id {
+                            Some(idx)
+                        } else {
+                            None
+                        }
+                    },
+                )
+            {
+                edges.swap_remove(idx);
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// (c) Copyright 2025 Mikołaj Kuranowski
+// SPDX-License-Identifier: MIT
+
+mod graph;
+
+pub use graph::Graph;
+
+/// Represents an element of the [Graph].
+///
+/// Due to turn restriction processing, one OpenStreetMap node
+/// may be represented by multiple Node instances. If that is the
+/// case, a "canonical" node (not bound by any turn restrictions) will
+/// have `id == osm_id`.
+///
+/// Nodes with `id == 0`, `osm_id == 0` or `osm_id >= 0x0008_0000_0000_0000`
+/// are disallowed. Zero IDs are used by the C bindings to signify absence of nodes,
+/// while large IDs are reserved for turn restriction processing.
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Node {
+    pub id: i64,
+    pub osm_id: i64,
+    pub lat: f32,
+    pub lon: f32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Represents an outgoing (one-way) connection from a specific [Node].
+///
+/// `cost` must be greater than the crow-flies distance between the two nodes.
+///
+/// Due to implementation details, `to` might not exist in the [Graph].
+/// Users must silently ignore such edges.
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Edge {
+    pub to: i64,
+    pub cost: f32,
 }
